e52261dd6b5dd0f1e84832fa0b63bd41a898a2a3
Brendan Leber <brendan@leberhall.com>
2013-07-06 12:41:12 -0700

    Initial version from the Old New Thing blog.

---
+++ stdafx.h
@@ -0,0 +1,18 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define STRICT
+#include <windows.h>
+#include <windowsx.h>
+#include <ole2.h>
+#include <commctrl.h>
+#include <shlwapi.h>
+#include <shlobj.h>
+#include <shellapi.h>
